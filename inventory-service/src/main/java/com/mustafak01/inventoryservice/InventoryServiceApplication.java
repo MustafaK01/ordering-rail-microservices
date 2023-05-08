34f6101e9b6df489cc1f2ea86c5088e5a893336e
MustafaK01 <mestefe119@gmail.com>
2023-05-08 15:10:29 +0300

    add inventory service

---
+++ inventory-service/src/main/java/com/mustafak01/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mustafak01.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}

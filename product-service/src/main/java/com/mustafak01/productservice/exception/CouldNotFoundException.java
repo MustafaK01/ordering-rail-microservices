875ec09f29e0ca05a079ee57fd1a755247358311
MustafaK01 <mestefe119@gmail.com>
2023-05-08 12:22:18 +0300

    add for exceptions

---
+++ product-service/src/main/java/com/mustafak01/productservice/exception/CouldNotFoundException.java
@@ -0,0 +1,12 @@
+package com.mustafak01.productservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class CouldNotFoundException extends RuntimeException{
+    public CouldNotFoundException(){
+        super("Couldn't found product");
+    }
+
+}

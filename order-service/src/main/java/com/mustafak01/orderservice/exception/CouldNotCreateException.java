a1ddb23f31f5a8cad56e310d1dfd87da4d681504
MustafaK01 <mestefe119@gmail.com>
2023-05-08 14:39:45 +0300

    add for exception

---
+++ order-service/src/main/java/com/mustafak01/orderservice/exception/CouldNotCreateException.java
@@ -0,0 +1,11 @@
+package com.mustafak01.orderservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class CouldNotCreateException extends RuntimeException {
+    public CouldNotCreateException(String message){
+        super(message);
+    }
+}
---
+++ order-service/src/main/java/com/mustafak01/orderservice/exception/GeneralExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.mustafak01.orderservice.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(x->{
+            String fieldName = ((FieldError)x).getField();
+            String errorMsg = x.getDefaultMessage();
+            errors.put(fieldName,errorMsg);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(CouldNotCreateException.class)
+    public ResponseEntity<?> couldNotCreateException(CouldNotCreateException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                this.adjustMessages(e));
+    }
+
+    @ExceptionHandler(CouldNotFoundException.class)
+    public ResponseEntity<?> couldNotFoundException(CouldNotFoundException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                this.adjustMessages(e));
+    }
+
+    private Map<String, String> adjustMessages(RuntimeException exception){
+        Map<String,String> errors = new HashMap<>();
+        errors.put("msg",exception.getMessage());
+        return errors;
+    }
+
+
+}

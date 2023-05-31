9435317205d7270131e988c4c895834975542ef3
MustafaK01 <mestefe119@gmail.com>
2023-05-31 03:09:18 +0300

    edit for inventory

--- inventory-service/src/main/java/com/mustafak01/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/mustafak01/inventoryservice/repository/InventoryRepository.java
@@ -3,5 +3,10 @@ package com.mustafak01.inventoryservice.repository;
 import com.mustafak01.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+
+    Optional<Inventory> findByCode(String code);
+
 }
--- inventory-service/src/main/java/com/mustafak01/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/mustafak01/inventoryservice/service/InventoryService.java
@@ -1,12 +1,21 @@
 package com.mustafak01.inventoryservice.service;
 
+import com.mustafak01.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class InventoryService {
 
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
     public boolean isInStock(String code){
-        return false;
+        return inventoryRepository.findByCode(code).isPresent();
     }
 
 }
